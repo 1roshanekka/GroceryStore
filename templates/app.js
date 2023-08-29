1d2ac5ea6f2da380d78234a59342b1e0fdeb5035
Roshan <1roshanekka@gmail.com>
2023-08-30 01:06:29 +0530

    functionality

---
+++ templates/app.js
@@ -0,0 +1,8 @@
+function appearanceDark() {
+    document.getElementById("view").href="darkStyle.css"
+}
+
+function appearanceLight() {
+    document.getElementById("view").href="lightStyle.css"
+}
+

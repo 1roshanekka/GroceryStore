4f1be1216f3287c83cb2540b07849e545d8c1817
Roshan <1roshanekka@gmail.com>
2023-08-31 23:32:50 +0530

    links

--- templates/app.js
+++ templates/app.js
@@ -1,8 +1,17 @@
 function appearanceDark() {
     document.getElementById("view").href="darkStyle.css"
+    document.getElementById("store").src="assets/DownloadOnAppStoreLight.svg"
 }
 
 function appearanceLight() {
     document.getElementById("view").href="lightStyle.css"
+    document.getElementById("store").src="assets/DownloadOnAppStoreDark.svg"
 }
 
+function storeImageDark() {
+    document.getEmementById("store").src=""
+}
+
+function storeImageLight() {
+    document.getElementById("store").src=""
+}

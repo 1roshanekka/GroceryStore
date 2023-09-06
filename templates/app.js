2fa01da67a21f6162a6e7772aa52e82dc499eaf2
Roshan <1roshanekka@gmail.com>
2023-09-06 19:21:49 +0530

    dark

--- templates/app.js
+++ templates/app.js
@@ -1,11 +1,16 @@
 function appearanceDark() {
     document.getElementById("view").href="darkStyle.css"
     document.getElementById("store").src="assets/DownloadOnAppStoreLight.svg"
+    document.getElementById("cart").src="assets/cartDark.png"
+    document.getElementById("profileIcon").src="assets/profileIconDark.png"
+    
 }
 
 function appearanceLight() {
     document.getElementById("view").href="lightStyle.css"
     document.getElementById("store").src="assets/DownloadOnAppStoreDark.svg"
+    document.getElementById("cart").src="assets/cart.png"
+    document.getElementById("profileIcon").src="assets/profileIcon.png"
 }
 
 function storeImageDark() {

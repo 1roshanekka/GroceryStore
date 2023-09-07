ce8b2a3a4d77711e0462cf1a53a78c4b35447167
Roshan <1roshanekka@gmail.com>
2023-09-07 21:25:47 +0530

    templating only for jinja within HTML
    stupid implementation

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 from flask import Flask
 from flask import render_template
 
+# without this rennder_template we cant use ->
+# return render_template("which file to load")
+
+# only possible is return "Hello, World :P"
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/") # this targets the templates folder 
 
-def home():
-    return render_template("/indexchange.html")
+def manager():
+    return render_template("managerLogin.html")
+def user():
+    return render_template("userLogin.html")
 
+# def userLoggedIn():
+#     return render_template(); 
+
+# Mac OSX Monterey (12.x) currently uses ports 5000 and 7000 for its Control centre hence the issue.
+# Try running your app from port other than 5000 and 7000
 
 if __name__ == "__main__":
     app.debug = True
-    app.run()
+    app.run(port=6969)
+    # app.run(debug=True)

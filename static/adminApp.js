71a5e7f5e33495b263e484b5df302ad8c3131f91
Roshan <1roshanekka@gmail.com>
2023-09-24 00:50:17 +0530

    copied code, would revert back and use some other logic

--- static/adminApp.js
+++ static/adminApp.js
@@ -1,3 +1,18 @@
+// function rotate() {
+//     var change = document.querySelector(".ss");
+
+//     change.style.transform = "rotate(45deg)";
+// }
+var rotated = false;
 function rotate() {
-    document.getElementById("rotateThis").setAttribute("class", "vroom")
+    var change = document.querySelector(".ss");
+    
+    change.style.transform = "rotate(45deg)";
+    if (rotated) {
+        change.style.transform = "rotate(0deg)";
+    } else {
+        change.style.transform = "rotate(45deg)";
+    }
+
+    rotated = !rotated; // Toggle the rotation state
 }

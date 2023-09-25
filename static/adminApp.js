2502a03290daa2fb48fc094e44635c05373c9452
Roshan <1roshanekka@gmail.com>
2023-09-25 22:51:41 +0530

    making drop menu pasted code

--- static/adminApp.js
+++ static/adminApp.js
@@ -16,3 +16,21 @@ function rotate() {
 
     rotated = !rotated; // Toggle the rotation state
 }
+
+
+function navMenu_js() {
+    document.getElementById("dropdown-listID").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.drop-button')) {
+      var dropdowns = document.getElementsByClassName("dropdown-list");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }

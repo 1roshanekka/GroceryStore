453cef4b814175f22e6853c0ac41828bf48923e8
Roshan <1roshanekka@gmail.com>
2023-10-13 23:24:07 +0530

    deploy render3
    changes gunicorn to fetch latest

--- start.sh
+++ start.sh
@@ -1,6 +1,18 @@
+# #!/bin/bash
+# app="tbs"
+# docker build -t ${app} .
+# docker run -d -p 56733:80 \
+#   --name=${app} \
+#   -v $PWD:/app ${app}
+
+
 #!/bin/bash
-app="tbs"
-docker build -t ${app} .
-docker run -d -p 56733:80 \
-  --name=${app} \
-  -v $PWD:/app ${app}
+
+# Activate your virtual environment (if applicable)
+source /venv/bin/activate
+
+# Navigate to the directory containing your Flask app
+cd /core
+
+# Start the Gunicorn server
+gunicorn -b 127.0.0.1:8000 wsgi:app
--- wsgi.py
+++ wsgi.py
@@ -6,19 +6,4 @@ from core.app import app
 if __name__ == "__main__":
     app.run()
 
-import sys
-import os
-
-# Add the path to your Flask application
-path = '/home/username/mysite'
-if path not in sys.path:
-    sys.path.append(path)
-
-# Import your Flask application
-from core.app import app as application  # Assuming your Flask app object is named 'app'
-
-# Optional: Set environment variables or configurations here, if needed
-
-# Optional: Configure logging or other settings here, if needed
-
-# IMPORTANT: Do not call app.run() here, as it will interfere with the WSGI server
+#---------------------------------------------------

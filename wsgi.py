5b6f79f415ec3881a947faeacd0c447ff3a70483
Roshan <1roshanekka@gmail.com>
2023-10-14 00:34:13 +0530

    deploy render8

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,10 @@
 # wsgi.py for production 
-from core.app import app
+# from core.app import app
 
 
-# wsgi.py is a Python file that serves as the entry point for the WSGI server, which is responsible for handling HTTP requests and passing them on to the Flask application.
-if __name__ == "__main__":
-    app.run()
+# # wsgi.py is a Python file that serves as the entry point for the WSGI server, which is responsible for handling HTTP requests and passing them on to the Flask application.
+# if __name__ == "__main__":
+#     app.run()
 #---------------------------------------------------
 
 # this was for local

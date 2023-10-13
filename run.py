51a92232f0c5469dc5c20ef93dad362858108ceb
Roshan <1roshanekka@gmail.com>
2023-10-13 23:15:37 +0530

    deploy render1

---
+++ run.py
@@ -0,0 +1,8 @@
+# run.py for local development
+
+from core.app import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,24 @@
-from app import app
+# wsgi.py for production 
+from core.app import app
 
+
+# wsgi.py is a Python file that serves as the entry point for the WSGI server, which is responsible for handling HTTP requests and passing them on to the Flask application.
 if __name__ == "__main__":
     app.run()
+
+import sys
+import os
+
+# Add the path to your Flask application
+path = '/home/username/mysite'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Import your Flask application
+from core.app import app as application  # Assuming your Flask app object is named 'app'
+
+# Optional: Set environment variables or configurations here, if needed
+
+# Optional: Configure logging or other settings here, if needed
+
+# IMPORTANT: Do not call app.run() here, as it will interfere with the WSGI server

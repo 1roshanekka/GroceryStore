7fa2a7e5bf2862166c7bf4abcb9e7ae8b7bfecfb
Roshan <1roshanekka@gmail.com>
2023-10-14 00:30:07 +0530

    deploy render7
    changes gunicorn to fetch latest

--- wsgi.py
+++ wsgi.py
@@ -6,3 +6,18 @@ from core.app import app
 if __name__ == "__main__":
     app.run()
 #---------------------------------------------------
+
+# this was for local
+
+
+# for gunicorn
+import sys
+import os
+
+# Add the path to your Flask application
+path = os.path.dirname(os.path.abspath(__file__))  # Replace with your actual path
+if path not in sys.path:
+    sys.path.append(path)
+
+# Import your Flask application
+from core.app import app as application  # Assuming your Flask app object is named 'app'
